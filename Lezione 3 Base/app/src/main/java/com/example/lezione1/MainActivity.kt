8b5442db3bcd9482f94e541dc4ed139e5d2a39cf
MatteoMocci <moccimatteo23@gmail.com>
2024-10-24 14:58:22 +0200

    Added Lezione 3 and Lezione 4

---
+++ Lezione 3 Base/app/src/main/java/com/example/lezione1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.lezione1
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val message = findViewById<TextView>(R.id.tvMessage)
+        val button = findViewById<Button>(R.id.btnLogin)
+        button.setOnClickListener{
+            message.text = "Benvenuto!"
+            button.visibility = View.INVISIBLE
+        }
+
+    }
+}
---
+++ Lezione 4 Base/app/src/main/java/com/example/lezione1/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.lezione1
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val message = findViewById<TextView>(R.id.tvMessage)
+        val button = findViewById<Button>(R.id.btnLogin)
+        button.setOnClickListener{
+            login()
+        }
+
+    }
+    fun login(){
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+}
